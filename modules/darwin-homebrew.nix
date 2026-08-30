3eee25eb2cd9e996cfd10ea6496c1302b617bbbb
shk <101378576+shk95@users.noreply.github.com>
2026-08-30 23:34:28 +0900

    feat(unixlike): add homebrew formula cowsay

--- modules/darwin-homebrew.nix
+++ modules/darwin-homebrew.nix
@@ -63,6 +63,7 @@ _: {
       brews = [
         # Broken on Darwin in the locked nixpkgs; Linux uses the shared HM list.
         "bettercap"
+        "cowsay"
         "displayplacer"
         "mas"
       ];
